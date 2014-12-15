41a4599f84fb7bf11df749c542516dce39e5dc42
Gordon Zheng <technix1@gmail.com>
2014-12-14 20:35:33 -0500

    now we can properly find ORFs in reverse and direct strands!

--- pset3/main.cpp
+++ pset3/main.cpp
@@ -20,48 +20,68 @@ const std::string CODON_STOP_2 = "TAG";
 const std::string CODON_STOP_3 = "TGA";
 // const int CODON_LENGTH = 3;
 
+std::string reverseStrand(std::string strandBases);
+
 class OpenReadingFrame {
 private:
   std::string bases;
   std::string direction;
 
-  int beginBaseIndex;
-  int endBaseIndex;
-  int readingFrame;
-  int numberInReadingFrame;
+  int beginBaseIndex;       // 0-based index of first base of ORF (from direct or reverse strand)
+  int endBaseIndex;         // 0-based index of last base of ORF (from direct or reverse strand)
+  int readingFrame;         // offset with which we read through the direct/rev strand to find this ORF.  Either 0, 1, or 2.  
+  int numberInReadingFrame; // within the readingFrame, which # ORF was this one?  Either 1 (first found in frame), 2, or 3
 
 public:
   OpenReadingFrame();
-  OpenReadingFrame(std::string bases);
+  OpenReadingFrame(std::string bases, std::string direction, int beginBaseIndex, int endBaseIndex, int readingFrame, int numberInReadingFrame);
 
   std::string translateToProteinSequence();
 };
+
+class Sequence {
+  private:
+    std::string description;
+    std::string basesDirect;
+    std::string basesReverse;
+
+    std::vector<OpenReadingFrame> openReadingFrames;
+
+  public:
+    Sequence(std::string description, std::string bases);
+
+    void findOpenReadingFrames();
+    void findORFsInFrame(int frame, bool reverse);
+};
+
+OpenReadingFrame::OpenReadingFrame(std::string bases, std::string direction, int beginBaseIndex, int endBaseIndex, int readingFrame, int numberInReadingFrame) {
+  this->bases = bases;
+  this->direction = direction;
+  this->beginBaseIndex = beginBaseIndex;
+  this->endBaseIndex = endBaseIndex;
+  this->readingFrame = readingFrame;
+  this->numberInReadingFrame = numberInReadingFrame;
+
+  std::cout << direction
+  << " " << beginBaseIndex + 1
+  << "-" << endBaseIndex + 1
+  << " RF[" << readingFrame + 1
+  << "] #" << numberInReadingFrame 
+  << " " << bases << std::endl;
+}
 
 std::string translateToProteinSequence() {
   return "foo";
 };
 
-class Sequence {
-	private:
-		std::string description;
-		std::string bases;
-
-		std::vector<OpenReadingFrame> openReadingFrames;
-
-	public:
-		Sequence(std::string description, std::string bases);
-		std::string getReverseStrand();
-
-		void findOpenReadingFrames();
-};
-
 Sequence::Sequence(std::string description, std::string bases) {
 	this->description = description;
-	this->bases = bases;
+	this->basesDirect = bases;
+  this->basesReverse = reverseStrand(bases);
 }
 
-std::string Sequence::getReverseStrand() {
-  std::string reverse = this->bases;
+std::string reverseStrand(std::string strandBases) {
+  std::string reverse = strandBases;
 
   for (int i = 0; i < reverse.length(); i++) {
     if (reverse[i] == 'A') { reverse[i] = 'T'; }
@@ -70,31 +90,50 @@ std::string Sequence::getReverseStrand() {
     else if (reverse[i] == 'G') { reverse[i] = 'C'; }
   }
 
-	return reverse;
+  return reverse;
 }
 
 void Sequence::findOpenReadingFrames() {
+  // TODO: do this for the reverse strand:
+  for (int i = 0; i < 3; i++) {
+    this->findORFsInFrame(i, false);
+    this->findORFsInFrame(i, true);
+  }
+}
 
-  bool withinFrame = false;
-  int frameBeginIndex;
-  std::string currentCodon;
+void Sequence::findORFsInFrame(int frame, bool reverse) {
+  bool withinFrame = false;   // true: we found a START and are currently looking for the END
+  int frameBeginIndex;        // position where we found the START codon
+  std::string currentCodon;   // buffer to store the current codon / substring of 3 continuous bases
+  int numberOfORFsFound = 0;
 
-  for (int i=0; i < this->bases.length() - 3; i += 3) {
-    currentCodon = this->bases.substr(i, 3);
+  std::string strand = this->basesDirect;
+
+  if (reverse == true) {      // if reverse strand, we will need to reverse the string (to read it backwards)
+    std::string reverseStrand = std::string(this->basesReverse.rbegin(), this->basesReverse.rend());
+    strand = reverseStrand;
+  }
+
+  // find ORFs from reading frame 1 (index 0)
+  for (int i = frame; i < strand.length() - 2; i += 3) {
+    currentCodon = strand.substr(i, 3);
 
     if ((currentCodon == CODON_START) && (withinFrame == false)) {
       frameBeginIndex = i;
       withinFrame = true;
-      // i += 2; // skip the next 2 bases (since they make up this codon)
+      i += 3;       // skip the next codon since the codon right after the start codon can't be the end codon. An ORF has a minimum length of 3 codons, including the start and end codons. 
 
     } else if  ((withinFrame == true) && 
                 (currentCodon == CODON_STOP_1 || 
                  currentCodon == CODON_STOP_2 || 
                  currentCodon == CODON_STOP_3)) {
       withinFrame = false;
+      numberOfORFsFound++;
 
-      std::string CRF = this->bases.substr(frameBeginIndex, (i + 3 - frameBeginIndex));
-      std::cout << "crf: " << CRF << std::endl;
+      std::string ORFBases = strand.substr(frameBeginIndex, (i + 3 - frameBeginIndex));
+      OpenReadingFrame newORF = OpenReadingFrame(ORFBases, reverse ? "reverse": "direct", frameBeginIndex, i + 2, frame, numberOfORFsFound);
+
+      this->openReadingFrames.push_back(newORF);
     }
   }
 
@@ -152,8 +191,6 @@ int main(int argc, char *argv[]) {
 	Sequence sequence = createSequenceFromFile(inputFileName);
 
 	sequence.findOpenReadingFrames();
-
-	// std::cout << sequence.getReverseStrand() << std::endl;
 
   return 0;
 }
